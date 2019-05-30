d0e7e5f1014bc6ae18369ab421fc41e9a62269f1
vaaceves <alexis@thinkof.tech>
2019-05-30 18:09:36 -0500

    pythoneverywhere update

--- webapp/manage.py
+++ webapp/manage.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2.7
 import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vitalq.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vitalquestions.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
